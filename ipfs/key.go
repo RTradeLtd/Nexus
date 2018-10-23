31dfbdfaea09ee1ef1eb863ca119f2a95d5a85e3
Robert Lin <robertlin1@gmail.com>
2018-10-23 12:21:35 -0700

    ipfs: don't panic on keygen failure

--- ipfs/key.go
+++ ipfs/key.go
@@ -3,15 +3,14 @@ package ipfs
 import (
 	"crypto/rand"
 	"encoding/hex"
-	"log"
+	"fmt"
 )
 
 // SwarmKey generates a new swarm key
 func SwarmKey() (string, error) {
 	key := make([]byte, 32)
-	_, err := rand.Read(key)
-	if err != nil {
-		log.Fatalln("While trying to read random source:", err)
+	if _, err := rand.Read(key); err != nil {
+		return "", fmt.Errorf("error generating key: %s", err.Error())
 	}
 	return "/key/swarm/psk/1.0.0/\n/base16/\n" + hex.EncodeToString(key), nil
 }
