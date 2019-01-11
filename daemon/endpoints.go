781f5141e7126b0d5cdd7ec6b4ea18ee685b2edf
Robert Lin <robertlin1@gmail.com>
2019-01-10 17:05:28 -0800

    daemon: add endpoint implementation for NetworkDiagnostics, update NetworkStats

--- daemon/endpoints.go
+++ daemon/endpoints.go
@@ -26,7 +26,6 @@ func (d *Daemon) StartNetwork(ctx context.Context,
 	}
 
 	return &ipfs_orchestrator.StartNetworkResponse{
-		Api:      n.API,
 		SwarmKey: n.SwarmKey,
 	}, nil
 }
@@ -53,23 +52,45 @@ func (d *Daemon) RemoveNetwork(ctx context.Context,
 }
 
 // NetworkStats retrieves stats about the requested node
-func (d *Daemon) NetworkStats(ctx context.Context,
-	req *ipfs_orchestrator.NetworkRequest) (*ipfs_orchestrator.NetworkStatusReponse, error) {
+func (d *Daemon) NetworkStats(
+	ctx context.Context,
+	req *ipfs_orchestrator.NetworkRequest,
+) (*ipfs_orchestrator.NetworkStatusReponse, error) {
 
 	s, err := d.o.NetworkStatus(ctx, req.Network)
-	if err != nil {
-		return nil, grpc.Errorf(codes.Internal, err.Error())
-	}
-	sb, err := json.Marshal(s.Stats)
 	if err != nil {
 		return nil, grpc.Errorf(codes.Internal, err.Error())
 	}
 
 	return &ipfs_orchestrator.NetworkStatusReponse{
 		Network:   req.Network,
-		Api:       s.API,
 		Uptime:    int64(s.Uptime),
 		DiskUsage: s.DiskUsage,
-		Stats:     sb,
+	}, nil
+}
+
+// NetworkDiagnostics retrieves detailed diagnostic details about the requested
+// network node
+func (d *Daemon) NetworkDiagnostics(
+	ctx context.Context,
+	req *ipfs_orchestrator.NetworkRequest,
+) (*ipfs_orchestrator.NetworkDiagnosticsResponse, error) {
+
+	s, err := d.o.NetworkDiagnostics(ctx, req.Network)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, err.Error())
+	}
+	nb, err := json.Marshal(s.NodeInfo)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, err.Error())
+	}
+	sb, err := json.Marshal(s.NodeStats)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, err.Error())
+	}
+
+	return &ipfs_orchestrator.NetworkDiagnosticsResponse{
+		NodeInfo: nb,
+		Stats:    sb,
 	}, nil
 }
